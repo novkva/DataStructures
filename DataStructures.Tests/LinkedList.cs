68c72d3b2b7b372b2cc1ae7e5d699f31527be5c0
Ira <brainforfreedom@gmail.com>
2020-11-18 02:17:05 +0300

    StartLinkedList

---
+++ DataStructures.Tests/LinkedList.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+
+namespace DataStructures.Tests
+{
+    class Class1
+    {
+        [TestCase(new int[] { 1, 2 , 5}, 6, new int[] {1, 2, 5, 6})]
+        [TestCase(new int[] { }, 6, new int[] { 6})]
+        [TestCase(new int[] { 1}, 6, new int[] {1, 6})]
+        [TestCase(new int[] { 1, 2 , 5, 4, 8 }, 6, new int[] {1, 2, 5, 4, 8,  6})]
+        public void Add(int[] array, int value, int[] expectedArray)
+        {
+            LinkedList expected = new LinkedList(expectedArray);
+            LinkedList actual = new LinkedList(array);
+            actual.Add(value);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(new int[] { 1, 2, 5 }, new int[] { 6 }, new int[] { 1, 2, 5, 6 })]
+        [TestCase(new int[] { }, new int[] { 6 }, new int[] { 6 })]
+        [TestCase(new int[] { 1 }, new int[] { 6, 7 }, new int[] { 1, 6 , 7})]
+        [TestCase(new int[] { 1, 2, 5, 4, 8 }, new int[] { }, new int[] { 1, 2, 5, 4, 8 })]
+        [TestCase(new int[] { 1, 2, 3}, new int[] {4, 5 }, new int[] { 1, 2,3, 4, 5})]
+        [TestCase(new int[] { }, new int[] { 8, 9}, new int[] { 8, 9 })]
+        [TestCase(new int[] { }, new int[] { }, new int[] { })]
+        public void AddArray(int[] array, int[] addArray, int[] expectedArray)
+        {
+            LinkedList expected = new LinkedList(expectedArray);
+            LinkedList actual = new LinkedList(array);
+            actual.Add(addArray);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(new int[] { 1, 2, 5 }, 6, new int[] {6, 1, 2, 5 })]
+        [TestCase(new int[] { }, 6, new int[] { 6 })]
+        [TestCase(new int[] { 1 }, 6, new int[] { 6, 1 })]
+        [TestCase(new int[] { 1, 2, 5, 4, 8 }, 10, new int[] { 10, 1, 2, 5, 4, 8 })]
+        public void AddToFirst(int[] array, int value, int[] expectedArray)
+        {
+            LinkedList expected = new LinkedList(expectedArray);
+            LinkedList actual = new LinkedList(array);
+            actual.AddToFirst(value);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(new int[] { 1, 2, 5 }, new int[] { 6 }, new int[] {6, 1, 2, 5 })]
+        [TestCase(new int[] { }, new int[] { 6 }, new int[] { 6 })]
+        [TestCase(new int[] { 1 }, new int[] { 6, 7 }, new int[] { 6, 7, 1 })]
+        [TestCase(new int[] { 1, 2, 5, 4, 8 }, new int[] { }, new int[] { 1, 2, 5, 4, 8 })]
+        [TestCase(new int[] { 1, 2, 3 }, new int[] { 4, 5 }, new int[] { 4, 5, 1, 2, 3  })]
+        [TestCase(new int[] { }, new int[] { 8, 9 }, new int[] { 8, 9 })]
+        [TestCase(new int[] { }, new int[] { }, new int[] { })]
+        public void AddArrayToFirst(int[] array, int[] addArray, int[] expectedArray)
+        {
+            LinkedList expected = new LinkedList(expectedArray);
+            LinkedList actual = new LinkedList(array);
+            actual.AddToFirst(addArray);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(new int[] { 10, 20, 30 }, 15, 1, new int[] { 10, 15, 20, 30 })]
+        [TestCase(new int[] { 44, 88, 99}, 77, 2, new int[] { 44, 88, 77, 99 })]
+        [TestCase(new int[] { 12, 13, 15 }, 10, 0, new int[] { 10, 12, 13, 15 })]
+        [TestCase(new int[] { 0, 1, 2, 3 }, 4, 4, new int[] { 0, 1, 2, 3, 4 })]
+        public void AddByIndex(int[] array, int value, int index, int[] expectedArray)
+        {
+            LinkedList expected = new LinkedList(expectedArray);
+            LinkedList actual = new LinkedList(array);
+            actual.AddByIndex(value, index);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(new int[] { 10, 20, 30, 40}, new int[] { 50 },4, new int[] { 10, 20, 30, 40, 50 })]
+        [TestCase(new int[] { 10, 20, 30, 40}, new int[] { 50, 60 }, 4, new int[] { 10, 20, 30, 40, 50, 60 })]
+        [TestCase(new int[] { 10, 20, 30, 40}, new int[] { 35}, 3, new int[] { 10, 20, 30,35, 40 })]
+        [TestCase(new int[] { 10, 20, 30, 40}, new int[] { 35, 37 }, 3, new int[] { 10, 20, 30, 35, 37, 40 })]
+        [TestCase(new int[] { }, new int[] { 6 },0, new int[] { 6 })]
+        [TestCase(new int[] { }, new int[] { 1, 10 },0, new int[] { 1, 10 })]
+        [TestCase(new int[] { }, new int[] {},0, new int[] { })]
+        [TestCase(new int[] { 11, 12, 13, 14 }, new int[] { 10}, 0, new int[] { 10, 11, 12, 13, 14 })]
+        [TestCase(new int[] { 11, 12, 13, 14 }, new int[] { 9, 10}, 0, new int[] {9, 10, 11, 12, 13, 14 })]
+        [TestCase(new int[] { 11, 12, 13, 14 }, new int[] { }, 0, new int[] { 11, 12, 13, 14 })]
+        [TestCase(new int[] { 10, 20, 30, 80, 90 }, new int[] { 40, 50 }, 3, new int[] { 10, 20, 30, 40, 50, 80, 90 })]
+        [TestCase(new int[] { 10, 20, 30, 80, 90 }, new int[] { 50 }, 3, new int[] { 10, 20, 30, 50, 80, 90 })]
+        [TestCase(new int[] { 10, 20, 30, 80, 90 }, new int[] { }, 3, new int[] { 10, 20, 30, 80, 90 })]
+        public void AddArrayByIndex(int[] array, int[] addArray, int index, int[] expectedArray)
+        {
+            LinkedList expected = new LinkedList(expectedArray);
+            LinkedList actual = new LinkedList(array);
+            actual.AddByIndex(addArray, index);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ DataStructures/LinkedList.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures
+{
+    public class LinkedList
+    {
+        public int Length {get; private set;}
+
+        private Node _root;
+
+        public LinkedList()
+        {
+            Length = 0;
+            _root = null;
+        }
+
+        public LinkedList(int[] array)
+        {
+            Length = array.Length;
+            if (Length != 0)
+            {
+                _root = new Node(array[0]);
+                Node crnt = _root;
+                for (int i = 1; i < Length; i++)
+                {
+                    crnt.Next = new Node(array[i]);
+                    crnt = crnt.Next;
+                }
+            }
+            else
+            {
+                _root = null;
+            }
+        }
+
+        public int this[int index]
+        {
+            get
+            {
+                Node tmp = _root;
+                for (int i = 0; i < index; i++)
+                {
+                    tmp = tmp.Next;
+                }
+                return tmp.Value;
+            }
+            set
+            {
+                Node tmp = _root;
+                for (int i = 0; i < index; i++)
+                {
+                    tmp = tmp.Next;
+                }
+                tmp.Value = value;
+            }
+        }
+
+        public void Add(int value)
+        {
+            Node crnt = _root;
+            for (int i = 0; i < Length - 1; i++)
+            {
+                crnt = crnt.Next;
+            }
+
+            Node tmp = new Node(value);
+            if (Length != 0)
+            {
+                crnt.Next = tmp;
+            }
+            else
+            {
+
+                _root = tmp;
+            }
+            Length++;
+        }
+
+        public void Add(int[] addArray)
+        {
+            Node crnt = _root;
+            for (int i = 0; i < Length - 1; i++)
+            {
+                crnt = crnt.Next;
+            }
+            LinkedList linkedList = new LinkedList(addArray);
+            if (Length != 0)
+            {
+                crnt.Next = linkedList._root;
+            }
+            else
+            {
+
+                _root = linkedList._root; 
+            }
+            
+            Length += addArray.Length;
+        }
+
+        public void AddToFirst(int value)
+        {
+            Node tmp = new Node(value);
+            tmp.Next = _root;
+            _root = tmp;
+            Length++;
+        }
+
+        public void AddToFirst(int[] addArray)
+        {
+            LinkedList linkedList = new LinkedList(addArray);
+            Node crnt = linkedList._root;
+            for (int i = 0; i < addArray.Length - 1; i++)
+            {
+                crnt = crnt.Next;
+            }
+            if (addArray.Length!=0) 
+            {
+                crnt.Next = _root;
+                _root = linkedList._root;
+            }
+            Length += addArray.Length;
+        }
+
+        public void AddByIndex(int value, int index)
+        {
+            if (index < 0 || index > Length)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            if (index != 0)
+            {
+                Node crnt = _root;
+                for (int i = 0; i < index - 1; i++)
+                {
+                    crnt = crnt.Next;
+                }
+                Node tmp = new Node(value);
+                tmp.Next = crnt.Next;
+                crnt.Next = tmp;
+                Length++;
+            }
+            else
+            {
+                AddToFirst(value);
+            }
+        }
+
+        public void AddByIndex(int[] addArray, int index)
+        {
+            if (index < 0 || index > Length)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            if (index != 0)
+            {
+                Node crnt = _root;
+                for (int i = 0; i < index - 1; i++)
+                {
+                    crnt = crnt.Next;
+                }
+
+                LinkedList linkedList = new LinkedList(addArray);
+
+                Node crntLL = linkedList._root;
+                for (int i = 0; i < addArray.Length-1; i++)
+                {
+                    crntLL = crntLL.Next;
+                }
+                if (addArray.Length != 0)
+                {
+                    crntLL.Next = crnt.Next;
+                    crnt.Next = linkedList._root;
+                }
+                Length += addArray.Length;
+            }
+            else
+            {
+                AddToFirst(addArray);
+            }
+        }
+
+        public override bool Equals(object obj)
+        {
+            LinkedList linkedList = (LinkedList)obj;
+            if (Length != linkedList.Length)
+            {
+                return false;
+            }
+
+            Node tmp = _root;
+            Node tmpObj = linkedList._root;
+            for (int i = 0; i < Length; i++)
+            {
+                if (tmp.Value != tmpObj.Value)
+                {
+                    return false;
+                }
+                tmp = tmp.Next;
+                tmpObj = tmpObj.Next;
+            }
+            return true;
+        }
+
+        public override string ToString()
+        {
+            string s = "";
+            Node tmp = _root;
+            for (int i = 0; i < Length; i++)
+            {
+                s += tmp.Value + ";";
+                tmp = tmp.Next;
+            }
+            return s;
+        }
+    }
+}
